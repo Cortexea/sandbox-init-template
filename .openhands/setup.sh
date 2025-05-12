2dfc01bba26ea793b253c7bf725d7921851f721c
Ishan Sharma <ishans996@gmail.com>
2025-05-12 14:55:21 +0530

    updated setup.sh

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "----- Begin setup.py -----"
 cd blockchain && npm install
+npx hardhat node 2>&1 > /tmp/hardhat_node.log &
 cd ../frontend && npm install 
 cd ..
 echo "-----   End setup.py -----"
