4ab4df3cbe2dbb14bc047e2c717637d75106ceca
Ishan Sharma <ishans996@gmail.com>
2025-05-16 20:31:55 +0530

    added sudo for global npm install (workaround)

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -2,8 +2,6 @@
 echo "----- Begin setup.py -----"
 cd blockchain && npm install
 npx hardhat node 2>&1 > /tmp/hardhat_node.log &
-echo "shishan debugging here too"
-whoami
-npm install -g http-server
+sudo npm install -g http-server
 cd ..
 echo "-----   End setup.py -----"
