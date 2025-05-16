45775dc2fadf727ff0119188f43b29eb4322afe4
Ishan Sharma <ishans996@gmail.com>
2025-05-16 19:14:19 +0530

    added http-server

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -2,5 +2,6 @@
 echo "----- Begin setup.py -----"
 cd blockchain && npm install
 npx hardhat node 2>&1 > /tmp/hardhat_node.log &
+npm install -g http-server
 cd ..
 echo "-----   End setup.py -----"
