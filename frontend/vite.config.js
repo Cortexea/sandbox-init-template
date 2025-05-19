c76ac4465868c33f207438c6c7c82bdcbc2d96d3
Pankaj Kumar Agarwal <43635589+Shankusu993@users.noreply.github.com>
2025-05-19 18:46:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::", // Allows IPv6 and external access (same as 0.0.0.0)
+    host: "0.0.0.0", // Allows IPv6 and external access (same as 0.0.0.0)
     port: 8080,
   },
 }));
